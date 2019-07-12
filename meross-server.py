3cedf7c862c2cc9f76afff56e482ef2676650337
Daniel Vivar <jdvivar@gmail.com>
2019-07-12 21:47:52 +0200

    Adding credentials creation for firebase

--- meross-server.py
+++ meross-server.py
@@ -6,7 +6,27 @@ from meross_iot.cloud.devices.door_openers import GenericGarageDoorOpener
 import time
 import os
 import sys
+import json
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+import datetime
 
+# Use a service account
+cred = credentials.Certificate({
+			  "type": "service_account",
+			  "project_id": os.environ.get('PROJECT_ID'),
+			  "private_key_id": os.environ.get('PRIVATE_KEY_ID'),
+			  "private_key": os.environ.get('PRIVATE_KEY').replace('\\n', '\n'),
+			  "client_email": os.environ.get('CLIENT_EMAIL'),
+			  "client_id": os.environ.get('CLIENT_ID'),
+			  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+			  "token_uri": "https://oauth2.googleapis.com/token",
+			  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+			  "client_x509_cert_url": os.environ.get('CLIENT_CERT_URL')
+		})
+firebase_admin.initialize_app(cred)
+db = firestore.client()
 
 EMAIL = os.environ.get('MEROSS_EMAIL')
 PASSWORD = os.environ.get('MEROSS_PASSWORD')
@@ -46,6 +66,8 @@ if __name__=='__main__':
 
     while plug.supports_electricity_reading():
         print("Current consumption is: %s" % str(plug.get_electricity()))
+        doc_ref = db.collection(u'plug-logs').add(plug.get_electricity())
+        doc_ref.set({ u'date': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") })
         sys.stdout.flush()
         time.sleep(5)
 
